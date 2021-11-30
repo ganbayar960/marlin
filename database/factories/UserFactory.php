72566ea2449a168690aef1fd00594e336958891a
Ganbayar <ganbayar960@gmail.com>
2021-11-30 18:07:40 +1100

    Added DB setups (User for User list, Type for User type): 	- Created migrations for create table in the DB 	- Created Models for the Table 	- Created Factoreis and seed for dummy data

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $type_id = rand(1,10);
+
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'type_id' => $type_id,
+            'last_logged_at' => now(),
+            'authorized_at' => now(),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function unverified()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'email_verified_at' => null,
+            ];
+        });
+    }
+}
