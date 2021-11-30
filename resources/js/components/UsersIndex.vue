210c72eb63cf015359b4877d7b88b153ec5eb2cf
Ganbayar <ganbayar960@gmail.com>
2021-11-30 21:23:53 +1100

    Added modal with UserType list and Created API for get UserType list.

--- resources/js/components/UsersIndex.vue
+++ resources/js/components/UsersIndex.vue
@@ -121,6 +121,7 @@ export default {
     },
     mounted(){
         this.getUsers();
+        this.getTypes();
     }
 };
 
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\TypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/users', [UserController::class, 'index']);
+Route::get('/types', [TypeController::class, 'index']);
